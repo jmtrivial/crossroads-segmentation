4518b93fb598a48f2491741554f557a50531921b
Jean-Marie Favreau <j-marie.favreau@uca.fr>
2021-10-15 12:48:43 +0200

    Still working from another path

--- src/get-crossroad-description.py
+++ src/get-crossroad-description.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 import argcomplete, argparse
-
+import os
 
 import networkx as nx
 import osmnx as ox
@@ -18,7 +18,7 @@ import crseg.region as rg
 import json
 
 # load predefined crossroad coordinates
-with open("crossroads-by-name.json") as f:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "crossroads-by-name.json")) as f:
     coordsByName = json.load(f)
 
 
